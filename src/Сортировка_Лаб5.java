75ae6bcd6a2f2bcd5bd57b5e87d1464ded31f293
l-Mel-l <125726970+l-Mel-l@users.noreply.github.com>
2023-03-06 08:46:06 +0300

    Initial commit

---
+++ src/Сортировка_Лаб5.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+public class Сортировка_Лаб5 {
+    public static void main(String[] args) {
+
+        // Задаётся массив
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите размер массива");
+        int n = sc.nextInt();
+        int[] mass = new int[n];
+        System.out.println("Введите значения массива");
+        for (int i = 0; i < n; i++) {
+            mass[i] = sc.nextInt();
+        }
+        sort(mass);
+        for (int i = 0; i < n; i++) {
+            System.out.println(mass[i]);
+        }
+    }
+        //Сортировочка
+
+        public static void sort(int[]mass){
+        for (int i = 1; i < mass.length - 1; i++) {
+            for (int j = 0; j < mass.length - 1; j++) {
+                if (mass[j + 1] < mass[j]) {
+                    int temp = mass[j];
+                    mass[j] = mass[j + 1];
+                    mass[j + 1] = temp;
+                }
+            }
+
+        }
+
+
+    }
+}
